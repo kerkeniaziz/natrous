b3b9639878388e4dd0004f289933c71d0215c7a7
kerkeniaziz <kerkeniaziz@gmail.com>
2024-02-04 23:39:56 +0100

    Handling GET Requests Done

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.status(200).json({message : 'Hello World', app:'Natrous'});
     res.end();
     
@@ -12,6 +12,19 @@ app.post('/', (req, res) => {
     res.status(200).json({message : 'you can post to this endpoint ...', app:'Natrous'});
     res.end();
     
+});*/
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status : 'success', 
+        results : tours.length,
+        app:'Natrous',
+        data : {
+            tours
+        },
+        
+    });
 });
 
 const port = 3000;
