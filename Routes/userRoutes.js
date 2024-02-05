3e05104eccab74e45913c2e9c8b588850e4cf3a7
kerkeniaziz <kerkeniaziz@gmail.com>
2024-02-05 22:37:33 +0100

    fixing

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const userController = require("../controller/userController");
 
 const Router = express.Router();
-
+ 
 Router
     .route('/')
     .get(userController.getAllUser)
