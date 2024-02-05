2b0658a1cd3e8076c31465562e9a15d0a5d8510c
kerkeniaziz <kerkeniaziz@gmail.com>
2024-02-05 01:05:15 +0100

    patch request

--- app.js
+++ app.js
@@ -71,6 +71,19 @@ app.post('/api/v1/tours', (req, res) => {
 
 } )
 
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id *1;
+
+    if (id> tours.length) {
+        return res.status(404).json({
+            status : 'fail',
+            message : 'invalid id',
+        });
+    }
+    console.log(req.body);
+    res.status(200).json({status : 'success',data : {tour :'update tour here'}});
+});
 const port = 3000;
 app.listen(port,() => {
     console.log(`Server is running on port ${port}`);
