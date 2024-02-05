631d31771b2d2640c108236d0eaca9c14f781baf
kerkeniaziz <kerkeniaziz@gmail.com>
2024-02-05 01:46:06 +0100

    using 3rd party middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const morgan = require('morgan');
 
+
+app.use(morgan('dev'));
 app.use(express.json());
+
 
 app.use((req, res, next) => {
     console.log('Hello from the middleware');
